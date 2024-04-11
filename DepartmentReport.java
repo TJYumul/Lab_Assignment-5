65c52f315aa64d19527b70ef908e969bcb3fcc16
TJ Yumul <132363388+TJYumul@users.noreply.github.com>
2024-04-11 23:46:08 +0800

    Add files via upload

--- DepartmentReport.java
+++ DepartmentReport.java
@@ -1,14 +1,10 @@
 
-import java.util.Comparator;
-import java.util.TreeMap;
+import java.io.IOException;
+
 
 public class DepartmentReport {
-    public static void main(String[] args) {
-        DepartmentDA departmentDA = new DepartmentDA();
-        TreeMap<String, Department> sortedDepartmentMap = new TreeMap<>(Comparator.reverseOrder());
-        sortedDepartmentMap.putAll(departmentDA.getDepartmentMap());
-        for (Department department : sortedDepartmentMap.values()) {
-            departmentDA.printDepartment(department);
-        }
+    public static void main(String[] args) throws IOException {
+        new DepartmentDA();
+
     }
 }
